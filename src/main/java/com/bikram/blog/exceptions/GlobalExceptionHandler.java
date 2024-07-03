dde80ff1827f166022c8b10bb2ff559d33f7a13c
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2024-07-04 00:13:57 +0530

    Added Validations to the User APIs
    - Added Validations to the User APIs
    - Also renamed the Test File Name from 'DemoApplicationTest.java' to 'BlogApplicationTest.java' as the main application filename is also changed from 'DemoApplicarion.java' to 'BlogApplication.java'

--- src/main/java/com/bikram/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bikram/blog/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.bikram.blog.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,12 +15,32 @@ import com.bikram.blog.payloads.ApiResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	// for handling ResourceNotFoundException (custom exception)
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
 		String message = ex.getMessage();
 		ApiResponse apiResponse = new ApiResponse(message,false);
 		return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	
+	
+	// for handling MethodArgumentNotValidException
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 		
+		// creating new HashMap object
+		Map<String, String> response = new HashMap<>();
+		
+		// getting all errors from validation exception
+		ex.getBindingResult().getAllErrors().forEach((error)->{ // traversing each errors 
+			String fieldName = ((FieldError)error).getField(); // type-casting the error to fieldError and fetching field from the fieldError
+			String message = error.getDefaultMessage(); // fetching message from the error
+			response.put(fieldName, message); // adding the key (fieldName) and value (message) pair to build the response
+		});
+		
+		// return the response (hashMap object) with status code BAD_REQUEST
+		return new ResponseEntity<Map<String,String>>(response,HttpStatus.BAD_REQUEST); 
 	}
 	
 }
--- src/main/java/com/bikram/blog/payloads/UserDto.java
+++ src/main/java/com/bikram/blog/payloads/UserDto.java
@@ -1,5 +1,8 @@
 package com.bikram.blog.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +13,19 @@ import lombok.Setter;
 public class UserDto {
 
 	private int userId;
+	
+	@NotEmpty(message = "userName is required !!")
+	@Size(min=4,message = "userName must contain atleast 4 characters !!")
 	private String userName;
+	
+	@Email(message = "userEmail is not valid !!")
+	@NotEmpty(message = "userEmail is required !!")
 	private String userEmail;
-	private String userPassword;
+	
+	@NotEmpty(message = "userPassword is required !!")
+	@Size(min = 3, max = 10, message = "userPassword must contain atleast 3 and atmost 10 characters !!")
+	private String userPassword; // we can also add some pattern using "regex" for password validation
+	
+	@NotEmpty(message = "userAbout is required !!")
 	private String userAbout;
 }
