4d2e14edb1abf423804928dbeb14a020cd5ee773
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2024-07-14 00:50:51 +0530

    added extra optional code for encoding a particular password
    - added extra optional code for encoding a particular password
    - this code can be used/written if we want to encode a particular password and get the value of encoded password on the console
    - password will be shown on the console

--- src/main/java/com/bikram/blog/BlogApplication.java
+++ src/main/java/com/bikram/blog/BlogApplication.java
@@ -1,13 +1,21 @@
 package com.bikram.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner{ // implemented the interface 'CommandLineRunner' to encode a particular password
 
+	// for the purpose of encoding a particular password
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
@@ -16,6 +24,13 @@ public class BlogApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	
+	// This method is coming due to implement the interface 'CommandLineRunner' | whenever the application will run, this method will also be running | 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("$Hey_&")); // "$Hey_&" is the password which we can encode | we will get the encoded password on the console, whenever we'll run the application
 	}
 
 }
