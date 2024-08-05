10247d8885daa5c035229898fbe53f49f25486a3
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2024-08-05 15:58:31 +0530

    added Swagger Configuration with Spring Security for API Docs
    added Swagger Configuration with Spring Security for API Docs

--- src/main/java/com/bikram/blog/config/SwaggerConfig.java
+++ src/main/java/com/bikram/blog/config/SwaggerConfig.java
@@ -5,19 +5,32 @@ import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 @Configuration
 public class SwaggerConfig {
 
+	String schemeName = "bearerScheme";
+	
 	// method to return OpenAPI object
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement().addList(schemeName))
+				.components(new Components()
+						.addSecuritySchemes(schemeName, new SecurityScheme()
+								.name(schemeName)
+								.type(Type.HTTP)
+								.bearerFormat("JWT")
+								.scheme("bearer")))
 				.info(
 					new Info()
 						.title("Blogging Appilacation")
